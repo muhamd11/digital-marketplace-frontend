190a973b70a26a58b61706acb345ff5705b61e45
Muhamd Medhat <muhamed.medhat11@gmail.com>
2024-02-10 19:00:48 +0200

    some changes

--- app/checkout/page.jsx
+++ app/checkout/page.jsx
@@ -1,9 +1,9 @@
-i have this error "useRouter is not defined" with this code ""use client";
 import React from "react";
 import { Elements } from "@stripe/react-stripe-js";
 import { loadStripe } from "@stripe/stripe-js";
 import CheckoutForm from "./_components/CheckoutForm";
-import { useSearchParams } from "next/router"; // Use from 'next/router' instead of 'next/navigation'
+import { useRouter } from "next/router"; 
+
 
 const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHER_KEY);
 
@@ -25,37 +25,3 @@ const CheckOut = () => {
 };
 
 export default CheckOut;
-"
-The error "useRouter is not defined" is occurring because you are trying to use the useRouter hook without importing it from the next/router package. To fix this error, you need to import the useRouter hook at the top of your file.
-
-Here's the updated code with the useRouter hook imported and used:
-
-import React from "react";
-import { Elements } from "@stripe/react-stripe-js";
-import { loadStripe } from "@stripe/stripe-js";
-import CheckoutForm from "./_components/CheckoutForm";
-import { useRouter } from "next/router"; // Import the useRouter hook from next/router
-
-const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHER_KEY);
-
-const CheckOut = () => {
-  const router = useRouter(); // Use useRouter to access the search parameters
-  const searchParams = new URLSearchParams(router.asPath);
-  const options = {
-    mode: "payment",
-    currency: "usd",
-    amount: Number(searchParams.get("amount")) || 1,
-  };
-
-  return (
-    <Elements stripe={stripePromise}>
-      <CheckoutForm options={options} />{" "}
-      {/* Pass options as props to CheckoutForm */}
-    </Elements>
-  );
-};
-
-export default CheckOut;
-By importing and using the useRouter hook, the "useRouter is not defined" error should be resolved.
-
-Ask GPT-4 Turbo
