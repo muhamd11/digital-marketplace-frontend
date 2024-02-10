83b262d9dcca333f2a5d362ea64e3f7a2a96170d
Muhamd Medhat <muhamed.medhat11@gmail.com>
2024-02-10 18:23:47 +0200

    some changes"

--- app/_components/ProductSection.jsx
+++ app/_components/ProductSection.jsx
@@ -18,9 +18,9 @@ const ProductSection = () => {
 
   return (
     <div className="px-10 md:px-20 mb-10">
-      <span class="relative flex justify-center my-10">
-        <div class="absolute inset-x-0 top-1/2 h-px -translate-y-1/2 bg-transparent bg-gradient-to-r from-transparent via-primary  to-transparent opacity-75"></div>
-        <span class="relative z-10 bg-white px-6 text-6xl text-primary tracking-wide">
+      <span className="relative flex justify-center my-10">
+        <div className="absolute inset-x-0 top-1/2 h-px -translate-y-1/2 bg-transparent bg-gradient-to-r from-transparent via-primary  to-transparent opacity-75"></div>
+        <span className="relative z-10 bg-white px-6 text-6xl text-primary tracking-wide">
           ONLINE  COURSES
         </span>
       </span>
